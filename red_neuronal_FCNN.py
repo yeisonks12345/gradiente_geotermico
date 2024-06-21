f7f04cba7ca67fd40b5b1e34b0d259d66a91a3b8
Yeison Conejo <yeisonks1234@hotmail.com>
2024-06-21 12:03:09 -0500

    se incluye red neuronal

--- red_neuronal_FCNN.py
+++ red_neuronal_FCNN.py
@@ -7,10 +7,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
 # r2 de 0.2404, MAE 3.3
-# Supongamos que 'data.csv' es el archivo que contiene tus datos con las variables necesarias
+# lectura de datos
 data = pd.read_csv('data_pre_norm.csv',sep=';')
 
-# Asumiendo que las variables predictoras son todas menos la última columna, que es el objetivo
+# separar en features y target
 features = data.iloc[:, :-1].values
 target = data.iloc[:, -1].values
 
@@ -48,5 +48,10 @@ r2 = r2_score(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 print(f'R² on test set: {r2}')
 print(f'Mean Absolute Error on test set: {mae}')
+
+# generar predicción con FCNN a todos los datos
+
+#data['prediccionfcnngg'] = model.predict(features)
+#data.to_csv('datosconfcnn.csv')
 
 
